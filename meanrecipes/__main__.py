19f4b7d46d3aa57bb81dc34e2639a4c192b58cb1
Kieran Kelk <kak3@st-andrews.ac.uk>
2015-10-10 12:40:18 +0100

    Set application/json mimetype for response.

--- meanrecipes/__main__.py
+++ meanrecipes/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,7 +11,9 @@ def recipe(term=None):
     ingredients = [[300.0, 'g', 'flour'], [400.5, 'kg', 'chocolate']]
     method = ['Step 1', 'Step 2']
 
-    return render_template('recipe.json', title=term, ingredients=ingredients, method=method)
+    resp = make_response(render_template('recipe.json', title=term, ingredients=ingredients, method=method))
+    resp.mimetype = 'application/json'
+    return resp
 
 if __name__ == '__main__':
 	app.run(debug = True)
