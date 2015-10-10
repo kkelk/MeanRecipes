062ebb5ecf53c778c2e1e67e5f070927cedeb78d
Kieran Kelk <kak3@st-andrews.ac.uk>
2015-10-10 14:56:42 +0100

    Fix JSON parsing bug, ingredients displayed correctly again.

--- meanrecipes/static/js/script.js
+++ meanrecipes/static/js/script.js
@@ -3,29 +3,28 @@ $('#suggestions').typed({
     typeSpeed: 50,
     backSpeed: 15,
     backDelay: 1500,
-    loop: true,
+    loop: true
 });
 
 $('#recipe-form').submit(function() {
-	get_recipe($('#recipe-title').val());
-	return false;
+    get_recipe($('#recipe-title').val());
+    return false;
 });
 
 function get_recipe(title) {
-	var url = '/recipe/search/' + title;
-	$.get(url, 
-	function(data) {
-		$('#ingredients').empty();
-		$('#method').empty();
+    var url = '/recipe/search/' + title;
+    $.get(url,
+        function(data) {
+            $('#ingredients').empty();
+            $('#method').empty();
 
-		data = JSON.parse(data);
-		$('#title').text("Recipe for " + data.title);
-for (i = 0; i < data.ingredients.length; i++) {
-	 	$('#ingredients').append('<li>' + data.ingredients[i][0] + data.ingredients[i][1] + " " + data.ingredients[i][2] + '</li>');
+            $('#title').text("Recipe for " + data.title);
+            for (i = 0; i < data.ingredients.length; i++) {
+                $('#ingredients').append('<li>' + data.ingredients[i][0] + data.ingredients[i][1] + " " + data.ingredients[i][2] + '</li>');
+            }
+            for (i = 0; i < data.method.length; i++) {
+                $('#method').append('<li>' + data.method[i] + '</li>');
+            }
+        }
+    );
 }
-		for (i = 0; i < data.method.length; i++) {
-			$('#method').append('<li>' + data.method[i] + '</li>');
-		}
-	});
-}
-
